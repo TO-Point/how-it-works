51ae683fc08acb7d017812f35726f69c66359309
Austin Thesing <athesing@point.com>
2023-11-21 15:23:33 -0700

    Finalized Cap calculation
    Switched from annual cap to prorated per month

--- src/chart.js
+++ src/chart.js
@@ -40,7 +40,7 @@ function performCalculations() {
 
       let shareOfAppreciation = (homeValueForYear - appreciationStartingAmount) * pointPercentage;
 
-      let capBasedRepayment = pointOffer * Math.pow(1.2, year); //pointOffer * Math.pow((1+(.2/12)), (year * 12))
+      let capBasedRepayment = pointOffer * Math.pow(1 + 0.2 / 12, year * 12);
 
       let shareBasedRepayment = shareOfAppreciation + pointOffer;
 
